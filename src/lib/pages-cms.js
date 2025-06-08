dac7d68f4971e46f33c229dcc31bc5214519f9d8
Ben Mitchinson <mitchinson.dev@gmail.com>
2025-06-08 18:58:43 -0500

    remove duplicate cms entries

--- src/lib/pages-cms.js
+++ src/lib/pages-cms.js
@@ -28,58 +28,6 @@ export const vinylCollections = [
 						]
 					}
 				]
-			},
-			{
-				artist: 'Rosevelt Highschool',
-				title: 'South Pacific 2',
-				itemRoute: 'south-pacific-two',
-				records: [
-					{
-						name: 'Record One',
-						photos: [
-							'/ed-collection/imgs/IMG_7775.jpg',
-							'/ed-collection/imgs/IMG_7776.jpg',
-							'/ed-collection/imgs/IMG_7777.jpg',
-							'/ed-collection/imgs/IMG_7778.jpg'
-						],
-						songs: [
-							{
-								title: 'Test Song 1',
-								mp3Src: '/ed-collection/songs/test.mp3'
-							},
-							{
-								title: 'Test Song 2',
-								mp3Src: '/ed-collection/songs/test.mp3'
-							}
-						]
-					}
-				]
-			},
-			{
-				artist: 'Rosevelt Highschool',
-				title: 'South Pacific 3',
-				itemRoute: 'south-pacific-three',
-				records: [
-					{
-						name: 'Record One',
-						photos: [
-							'/ed-collection/imgs/IMG_7775.jpg',
-							'/ed-collection/imgs/IMG_7776.jpg',
-							'/ed-collection/imgs/IMG_7777.jpg',
-							'/ed-collection/imgs/IMG_7778.jpg'
-						],
-						songs: [
-							{
-								title: 'Test Song 1',
-								mp3Src: '/ed-collection/songs/test.mp3'
-							},
-							{
-								title: 'Test Song 2',
-								mp3Src: '/ed-collection/songs/test.mp3'
-							}
-						]
-					}
-				]
 			}
 		]
 	}
