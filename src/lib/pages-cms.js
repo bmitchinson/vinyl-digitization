fe414e8ef38a80c1b1015ae8fae528b7487e8796
Ben Mitchinson <mitchinson.dev@gmail.com>
2025-06-09 00:22:19 -0500

    all images / songs linked

--- src/lib/pages-cms.js
+++ src/lib/pages-cms.js
@@ -4,26 +4,145 @@ export const vinylCollections = [
 		collectionRoute: 'ed-collection',
 		collectionItems: [
 			{
-				artist: 'Rosevelt Highschool',
+				artist: 'Rosevelt Highschool 1960',
 				title: 'South Pacific',
 				itemRoute: 'south-pacific',
 				records: [
 					{
-						name: 'Record One',
+						name: 'South Pacific 1 of 8',
+						photos: ['/ed-collection/imgs/sp-overture.jpg'],
+						songs: [
+							{
+								title: 'Overture / Dites-Moi / Cockeyed Optimist / Twin Soliloquies (Ed Bittle)',
+								mp3Src: '/ed-collection/songs/highschool_overture.mp3'
+							}
+						]
+					},
+					{
+						name: 'South Pacific 2 of 8',
+						photos: ['/ed-collection/imgs/sp-reprise-bali.jpg'],
+						songs: [
+							{
+								title:
+									"Reprise / Boar's Tooth Ceremonial / Younger Than Springtime / I'm In Love With A Wonderful Guy (Ed Bittle)",
+								mp3Src: '/ed-collection/songs/highschool_reprise_bali.mp3'
+							}
+						]
+					},
+					{
+						name: 'South Pacific 3 of 8',
+						photos: ['/ed-collection/imgs/sp-carefully-taught.jpg'],
+						songs: [
+							{
+								title: 'Carefully Taught / This Nearly Was Mine (Ed Bittle) / Finale Ultimo (Ed)',
+								mp3Src: '/ed-collection/songs/highschool_carefully_taught.mp3'
+							}
+						]
+					},
+					{
+						name: 'South Pacific 4 of 8',
+						photos: ['/ed-collection/imgs/sp-gonna-wash.jpg'],
+						songs: [
+							{
+								title:
+									"I'm Gonna Wash That Man Right Out / Some Enchanted Evening (Ed Bittle) / Reprise: Some Enchanted Evening / I'm in love with a wonderful guy",
+								mp3Src: '/ed-collection/songs/highschool_im_gonna_wash.mp3'
+							}
+						]
+					},
+					{
+						name: 'South Pacific 5 of 8',
+						photos: ['/ed-collection/imgs/sp-how-it-feels.jpg'],
+						songs: [
+							{
+								title:
+									"This Is How It Feels (Ed) / Encore: I'm Gonna Wash / Final Act 1 / Opening Act 2",
+								mp3Src: '/ed-collection/songs/highschool_this_is_how_it_feels.mp3'
+							}
+						]
+					},
+					{
+						name: 'South Pacific 6 of 8',
+						photos: ['/ed-collection/imgs/sp-nothin-like-dame.jpg'],
+						songs: [
+							{
+								title:
+									"There Is Nothing Like a Dame / Bali Ha'I, I'm gonna wash that man right out-a my hair",
+								mp3Src: '/ed-collection/songs/highschool_nothing_like_a_dame.mp3'
+							}
+						]
+					},
+					{
+						name: 'South Pacific 7 of 8',
+						photos: ['/ed-collection/imgs/sp-happy-talk.jpg'],
+						songs: [
+							{
+								title: 'Happy Talk / Honey Bun',
+								mp3Src: '/ed-collection/songs/highschool_happy_talk.mp3'
+							}
+						]
+					},
+					{
+						name: 'South Pacific 8 of 8',
+						photos: ['/ed-collection/imgs/sp-some-enchanted.jpg'],
+						songs: [
+							{
+								title: 'Some Enchanted Evening / Finaletto / Bloody Mary',
+								mp3Src: '/ed-collection/songs/highschool_some_enchanted.mp3'
+							}
+						]
+					}
+				]
+			},
+			{
+				artist: "Cayuga's Waiters",
+				title: 'Record',
+				itemRoute: 'cayuga-waiters',
+				records: [
+					{
+						name: 'Cayuga Waiters',
 						photos: [
-							'/ed-collection/imgs/IMG_7775.jpg',
-							'/ed-collection/imgs/IMG_7776.jpg',
-							'/ed-collection/imgs/IMG_7777.jpg',
-							'/ed-collection/imgs/IMG_7778.jpg'
+							'/ed-collection/imgs/cayuga-sidea.jpg',
+							'/ed-collection/imgs/cayuga-sideb.jpg'
 						],
 						songs: [
 							{
-								title: 'Side A',
-								mp3Src: '/ed-collection/songs/test.mp3'
+								title:
+									'Man is for the woman made / I could write a book / robinson crusoe / stormy weather / sentimental journey / blue world',
+								mp3Src: '/ed-collection/songs/red_bag_side_a.mp3'
 							},
 							{
-								title: 'Side B',
-								mp3Src: '/ed-collection/songs/test.mp3'
+								title:
+									"The day isn't long enough / this time the dream's on me / garden in the rain / when you wish upon a star / you are the girl I adore",
+								mp3Src: '/ed-collection/songs/red_bag_side_b.mp3'
+							}
+						]
+					}
+				]
+			},
+			{
+				artist: 'Sherwood',
+				title: 'Impressions',
+				itemRoute: 'impressions',
+				records: [
+					{
+						name: 'Impressions',
+						photos: [
+							'/ed-collection/imgs/sherwood-cover.jpg',
+							'/ed-collection/imgs/sherwood-rear.jpg',
+							'/ed-collection/imgs/sherwood-side1.jpg',
+							'/ed-collection/imgs/sherwood-side2.jpg'
+						],
+						songs: [
+							{
+								title:
+									'I married an angel / stormy weather / so sad and lonely / in the still of the night / man piaba / danny boy / san fernando',
+								mp3Src: '/ed-collection/songs/sherwood_impressions_side_a.mp3'
+							},
+							{
+								title:
+									"Do Da / I'll get alone / surrey with the fringe on top / cindy / high noon / fox / delia",
+								mp3Src: '/ed-collection/songs/sherwood_impressions_side_b.mp3'
 							}
 						]
 					}
